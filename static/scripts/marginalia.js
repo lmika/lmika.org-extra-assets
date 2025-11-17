844ae02684a3cfa4cdeb08cc249c8bcb405142be
Leon Mika <lmika@lmika.org>
2025-11-17 21:05:53 +1100

    Added marginalia boot script

---
+++ static/scripts/marginalia.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let ms = document.querySelectorAll('figure.marginalia');
+    for (let m of ms) {
+        let parentEl = m.parentElement;
+        parentEl.insertBefore(m, m.previousElementSibling);
+    }
+});
