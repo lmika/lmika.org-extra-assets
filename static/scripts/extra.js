52f185d025a5625f9a586bd5cd8fe63f2df61dde
Leon Mika <lmika@lmika.org>
2025-11-17 21:45:53 +1100

    Fixed placement script and display properties

--- static/scripts/extra.js
+++ static/scripts/extra.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let ms = document.querySelectorAll('figure.marginalia');
     for (let m of ms) {
         let parentEl = m.parentElement;
-        parentEl.insertBefore(m, m);
+        parentEl.insertBefore(m, m.previousElementSibling);
         m.classList.remove("hidden");
     }
 });
